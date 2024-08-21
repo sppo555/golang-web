f5caba422aaecd4507b533c4d60842942a02f30a
Alex.lin <alex.lin@letron.tech>
2024-08-21 18:06:50 +0800

    add env & timeout & db.ping

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 	"time"
+
+	"myproject/pkg/logger"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,23 +15,31 @@ import (
 var DB *sql.DB
 
 func init() {
-	log.SetFlags(0)
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	username := os.Getenv("TEST_DB_USERNAME")
-	password := os.Getenv("TEST_DB_PASSWORD")
-	dsn := fmt.Sprintf("%s:%s@tcp(34.97.41.216:3306)/test", username, password)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbName)
+
+	// 设置连接超时
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	var err error
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
-		log.Printf("%s 无法连接到数据库: %v", time.Now().Format(time.RFC3339), err)
+		logger.LogMessage(logger.ERROR, "无法打开数据库连接: %v", err)
 		os.Exit(1)
 	}
 
-	if err = DB.Ping(); err != nil {
-		log.Printf("%s 无法ping数据库: %v", time.Now().Format(time.RFC3339), err)
-		// os.Exit(1)
+	// 使用带超时的 Ping
+	err = DB.PingContext(ctx)
+	if err != nil {
+		logger.LogMessage(logger.ERROR, "无法连接数据库: %v", err)
+		os.Exit(1)
 	}
 
-	log.Printf("%s 成功连接到数据库", time.Now().Format(time.RFC3339))
+	logger.LogMessage(logger.INFO, "成功连接到数据库")
 }
